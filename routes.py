3c0a8e2a151a7e87c62d9cde4cd8c6d59fe607c1
Dehyu Sinyan <dehyu.sinyan@gmail.com>
2016-10-03 20:14:42 -0400

    Revert back to working code with no form

--- routes.py
+++ routes.py
@@ -18,17 +18,6 @@ callers = {
 @app.route("/", methods=['GET', 'POST'])
 def index():
  return render_template("index.html")
- from_number = request.values.get('From', None)
- if from_number in callers:
-  message = callers[from_number] + ", thanks for the message!"
- else:
-  message = "Monkey, thanks for the message!"
-
-
- resp = twilio.twiml.Response()
- resp.message(message)
-
- return str(resp)
 
 @app.route("/about")
 def about():
