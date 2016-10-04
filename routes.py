5cc7e2a2e978f551d28d8eecb41529e7a7360dcc
Dehyu Sinyan <dehyu.sinyan@gmail.com>
2016-10-04 17:59:07 -0400

    working changes

--- routes.py
+++ routes.py
@@ -1,7 +1,10 @@
+
 from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
 from forms import SignupForm
 from twilio.rest import TwilioRestClient
+from twilio import twiml
+
 
 app = Flask(__name__)
 
@@ -10,12 +13,42 @@ db.init_app(app)
 
 app.secret_key = "development-key"
 
+# The session object makes use of a secret key.
+SECRET_KEY = 'a secret key'
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+callers = {
+    "3013185581": "Curious George",
+    "14103018582": "Boots",
+    "+14158675311": "Virgil",
+}
+
 @app.route("/", methods=['GET', 'POST'])
-def index():
+def hello():
   return render_template("index.html")
-  resp = twilio.twiml.Response()
-  resp.message("Hello, Mobile Monkey")
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_response():
+  message_body = request.values.get('Body',None)
+  resp = twiml.Response()
+  if message_body == 'hello':
+    resp.message('Hello ' + message_body)    
+  elif message_body == 'no twili':
+    resp.message('Sorry to see you go. You can always re-enroll @ twili.io :)')
+
+
+
   return str(resp)
+
+
+
+
+   
+
+    
+
+
 
 @app.route("/about")
 def about():
@@ -29,7 +62,7 @@ def signup():
   client.messages.create(
       to="3013185581",
       from_="12407529966",
-      body="Hello there",
+      body="Well Hello there Chelbi",
       media_url="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/2/000/188/1fc/011d373.jpg",
     )
   return "Success!"
